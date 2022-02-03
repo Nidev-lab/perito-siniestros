20c6337033f2371a4c7637a693d6eb2f667b8905
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-02-03 00:20:10 -0300

    Structure project

---
+++ src/components/Alert/Alert.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './alert.css'
+
+const Alert = () => {
+  return (
+    <div class="alert alert-primary alert-border-radius" role="alert">
+      Example component.
+    </div>
+  )
+}
+ 
+export default Alert
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import Alert from "../../components/Alert/Alert";
+
+const Home = () => {
+  return (
+    <Alert />
+  );
+}
+ 
+export default Home;
