5efb3c7fdb29de342076dacc226800c610b0061c
Alejandro Busi <Alejandrobusi@gmail.com>
2022-02-16 13:27:58 -0300

    fix errors

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,11 @@
 import { Alert } from '../../components/Alert';
-import { Button } from '../../components/Button';
-import { Footer } from '../../components/Footer';
+import { Navbar } from '../../components/Navbar';
 
 const Home = () => {
   return (
     <>
-      <Alert />
-      <Button />
-      <Footer />
+    <Navbar />
+    <Alert />
     </>
   );
 };
