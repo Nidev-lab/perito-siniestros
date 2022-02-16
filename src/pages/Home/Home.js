eb4aa47a4fa67e7e452500bc085d3c3cd53cd476
Alejandro Busi <Alejandrobusi@gmail.com>
2022-02-16 17:31:43 -0300

    Fix errors (identation, fragment and tag img

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import { Alert } from '../../components/Alert';
 import { Navbar } from '../../components/Navbar';
+import { Footer } from '../../components/Footer';
 
 const Home = () => {
   return (
     <>
     <Navbar />
     <Alert />
+    <Footer />
     </>
   );
 };
