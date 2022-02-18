04ba8710b500ee0eab4ffbb7bd750f69ba07188c
Patricio García Millán <patogarciamillan@gmail.com>
2022-02-17 22:40:47 -0300

    Fix slint error in main

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,6 @@ import { Contact } from '../../components/Contact';
 import { Navbar } from '../../components/Navbar';
 import { Carousel } from '../../components/Carousel';
 
-
 const Home = () => {
   return (
     <>
