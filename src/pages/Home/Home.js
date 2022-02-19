0a6f38eb1b2fe02f90707a94b14366510492b5d4
cajimenez96 <carlosj_utn@hotmail.com>
2022-02-19 15:27:35 -0300

    Budget added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,14 +2,15 @@ import { Alert } from '../../components/Alert';
 import { Contact } from '../../components/Contact';
 import { Navbar } from '../../components/Navbar';
 import { Carousel } from '../../components/Carousel';
-
+import { Budget } from '../../components/Budget';
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <Contact />
       <Carousel />
+      <Budget />
+      <Contact />
       <Alert />
     </>
   );
