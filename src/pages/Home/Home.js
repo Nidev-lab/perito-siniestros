de2e0a87b57239da9e4ae135dbc161a9632b878b
Patricio García Millán <patogarciamillan@gmail.com>
2022-02-23 18:29:48 -0300

    Manage Sinister Added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,9 @@
 import { Navbar } from '../../components/Navbar';
 import { Contact } from '../../components/Contact';
 import { Carousel } from '../../components/Carousel';
-import { Footer } from '../../components/Footer'
+import { Footer } from '../../components/Footer';
+import { ManageSinister } from '../../components/ManageSinister';
+
 const Home = () => {
   return (
     <>
@@ -9,6 +11,7 @@ const Home = () => {
       <Contact />
       <Carousel />
       <Footer />
+      <ManageSinister />
     </>
   );
 };
