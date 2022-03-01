cda97a02ad7e83d3fe449142d7ded1593ccdc8f7
Patricio García Millán <patogarciamillan@gmail.com>
2022-03-01 18:33:27 -0300

    Fix errors

--- src/components/FreeRepair/FreeRepair.js
+++ src/components/FreeRepair/FreeRepair.js
@@ -9,9 +9,9 @@ const FreeRepair = () => {
           <img src="..." alt="img" />
           <div className=''>
             <p className='text-center'>Tené en cuenta que el seguro del tercero va a responder sólo en el caso en que su
-              asegurado (quien te chocó) tenga su póliza vigente al momento del siniestro y sea
-              el responsable de la ocurrencia del accidente y haya denunciado el mismo
-              ante la compañía.</p>
+            asegurado (quien te chocó) tenga su póliza vigente al momento del siniestro y sea
+            el responsable de la ocurrencia del accidente y haya denunciado el mismo
+            ante la compañía.</p>
           </div>
         </div>
       </div>
@@ -21,13 +21,13 @@ const FreeRepair = () => {
         </div>
         <div className='col-xl-6'>
           <p>Si el culpable del accidente automovilístico es el conductor del otro vehículo
-            (comúnmente llamando `el tercero`), los daños materiales y las lesiones que hayas
-            sufrido, deberían ser indemnizados por la aseguradora de quien te chocó...</p>
+          (comúnmente llamando `el tercero`), los daños materiales y las lesiones que hayas
+          sufrido, deberían ser indemnizados por la aseguradora de quien te chocó...</p>
           <p>... siempre y cuando, el tercero tenga un segundo contratado, con cobertura al día
-            y haya realizando la denuncia administrativa en su compañía de seguro.</p>
+          y haya realizando la denuncia administrativa en su compañía de seguro.</p>
         </div>
-          <p className='text-center'>¿Sabías que podés vender tu auto chocado?</p>
-          <button className='btn btn-success' type="button">Click aquí para mas info</button>
+        <p className='text-center'>¿Sabías que podés vender tu auto chocado?</p>
+        <button className='btn btn-success' type="button">Click aquí para mas info</button>
       </div>
     </div>
   );
