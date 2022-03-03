13bdc59b41c534eddb568a389ca7895f6f5f1535
Patricio García Millán <patogarciamillan@gmail.com>
2022-03-03 17:49:43 -0300

    Fix section FreeRepair

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,51 +1,9 @@
 import React from 'react';
-import logo from './images/logo.jpeg';
 
 const Budget = () => {
   return (
-      <div className='container d-flex flex-column'>
+      <div>
         <h2>Presupuesto</h2>
-        <div className="d-flex justify-content-center">
-          <div className="col-2">
-            <img src={logo} className='rounded mx-auto d-block w-50' />
-          </div>
-          <div>
-            <h5 className='fw-bold'>¿No tenés seguro?</h5>
-            <span className='fw-normal'>Nos encargamos de la gestión completa ante la compañía de seguro del tercero para que puedas reparar tu vehículo sin costo. Reclamá a la compañía del tercero aunque no tengas seguro.</span>
-            <br/>
-            <span className='fst-italic mt-4'>Te brindamos el mejor asesoramiento!. Contamos con presupuesto originales con firma responsable de un taller registrado brindando siempre valores actualizados en el mercado.</span>
-          </div>
-        </div>
-        <div className='d-flex flex-column'>
-          <div className="m-3">
-            <h5>Condiciones</h5>
-            <div>
-              <span className='fw-normal'>Para que podamos reparar tu vehículo el tercero (quien te chocó), tiene que cumplir con lo siguiente:</span>
-              <ul>
-                <li>Haber realizado la denuncia administrativa en su seguro.</li>
-                <li>Contar con cobertura al momento del siniestro.</li>
-                <li>Ser el responsable del accidente.</li>
-              </ul>
-            </div>
-          </div>
-          <div className="d-flex flex-wrap m-3">
-            <div>
-              <h5>Documentacion necesaria:</h5>
-              <div>
-                <ul>
-                  <li>Copia de Cédula Verde y Registro de conducir.</li>
-                  <li>Copia de DNI del titular.</li>
-                  <li>Denuncia Administrativa en tu seguro.</li>
-                  <li>Certificado de cobertura a la fecha del siniestro emitido por tu seguro.</li>
-                  <li>Fotos de los daños (una que se vea la patente).</li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-12 d-flex justify-content-end">
-              <input type="button" value="Solicitar asesoramiento" className="btn btn-success"/>
-            </div>
-          </div>
-        </div>
       </div>
   );
 };
--- src/components/FreeRepair/FreeRepair.js
+++ src/components/FreeRepair/FreeRepair.js
@@ -3,11 +3,11 @@ import React from 'react';
 const FreeRepair = () => {
   return (
     <div>
-      <h1>Repará tu auto sin costo</h1>
+      <h3 className='container mt-4'>Deseo Reparar mi auto sin costo</h3>
       <div className='d-flex justify-content-center aling-center'>
         <div className='d-flex flex-column w-75'>
-          <img src="..." alt="img" />
-          <div className=''>
+          <div>
+            <h1 className='text-center'>IMAGEN</h1>
             <p className='text-center'>Tené en cuenta que el seguro del tercero va a responder sólo en el caso en que su
             asegurado (quien te chocó) tenga su póliza vigente al momento del siniestro y sea
             el responsable de la ocurrencia del accidente y haya denunciado el mismo
@@ -26,8 +26,31 @@ const FreeRepair = () => {
           <p>... siempre y cuando, el tercero tenga un segundo contratado, con cobertura al día
           y haya realizando la denuncia administrativa en su compañía de seguro.</p>
         </div>
-        <p className='text-center'>¿Sabías que podés vender tu auto chocado?</p>
-        <button className='btn btn-success' type="button">Click aquí para mas info</button>
+          <p className='text-center m-4'>Te brindamos el mejor asesoramiento!. Contamos con presupuesto originales con firma responsable de un taller registrado brindando siempre valores actualizados en el mercado.</p>
+        <div>
+          <h5 className='ms-5'>Condiciones</h5>
+          <div className='ms-5 mt-4'>
+            <p>Para que podamos reparar tu vehículo el tercero (quien te chocó),
+            tiene que cumplir con lo siguiente:</p>
+            <ul>
+              <li>Haber realizado la denuncia administrativa en su seguro.</li>
+              <li>Contar con cobertura al momento del siniestro.</li>
+              <li>Ser el responsable del accidente.</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <h5 className='ms-5'>Documentacion necesaria:</h5>
+            <ul className='ms-5 mt-4'>
+              <li>Copia de Cédula Verde y Registro de conducir.</li>
+              <li>Copia de DNI del titular.</li>
+              <li>Denuncia Administrativa en tu seguro.</li>
+              <li>Certificado de cobertura a la fecha del siniestro emitido por tu seguro.</li>
+              <li>Fotos de los daños (una que se vea la patente).</li>
+            </ul>
+        </div>
+        <p className='text-center mt-4'>¿Sabías que podés vender tu auto chocado?</p>
+        <button className='btn btn-success w-50 mb-5' type="button">Click aquí para mas info</button>
       </div>
     </div>
   );
