542c1275a8c1e4865259accb34539a031364ede2
cajimenez96 <carlosj_utn@hotmail.com>
2022-03-28 23:11:59 -0300

    fix hero

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import { Hero } from '../../components/Hero';
 
 const Home = () => <Hero />;
-=======
-import { Carousel } from '../../components/Carousel';
-
-const Home = () => <Carousel />;
->>>>>>> 70e69f3ad2023f44a9618c7a1509294fe7fbc26c
 
 export default Home;
