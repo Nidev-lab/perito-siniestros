9c555a6518d95e84d89b8f85aaf61b2bae52b574
cajimenez96 <carlosj_utn@hotmail.com>
2022-03-30 15:09:30 -0300

    fixed hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -29,52 +29,48 @@ const Carousel = () => {
           <span className="visually-hidden">Next</span>
         </button>
       </div>
-      <div className="container d-flex justify-content-evenly">
-        <div className="d-flex align-items-center justify-content-center flex-column">
-          <div className="d-flex justify-content-center flex-column mb-3 mx-3">
+      <div className="d-flex justify-content-evenly mt-5">
+        <div className="col-md-5 d-flex align-items-center justify-content-center flex-column mb-5 pb-5">
+          <div className="d-flex justify-content-center flex-column">
             <p className="fw-normal">Un accidente de tránsito es siempre una experiencia difícil. A veces, el miedo y el estrés nos impiden reaccionar adecuadamente. Por ese motivo, es muy importante saber de antemano, qué pasos debemos seguir.</p>
-            <p className="fw-normal"><b>Hacer lo adecuado nos ayuda a resolver más fácil la situación.</b></p>
+            <p className="fw-normal text-center"><b>Hacer lo adecuado nos ayuda a resolver más fácil la situación.</b></p>
           </div>
           <img src={logoHero} width="150" />
         </div>
-        <div className="w-100">
-          <p>
-            <button className="btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample" title="Haz click para ver las sugerencias">
-              Sugerencias
-            </button>
-          </p>
+        <div className="col-md-5">
+          <button className="btn w-100 alert-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample" title="Haz click para ver las sugerencias">
+              <i>Sugerencias</i>
+          </button>
           <div className="collapse" id="collapseWidthExample">
             <div className="card card-body w-100">
-              <ul className="list-unstyled">
-                <li className="ms-2">
-                  <img src={listItem} className="pe-2"/>
-                  <span className="fw-normal">Comprobá que los pasajeros de tu auto no tengan lesiones.</span>
-                </li>
-                <li className="ms-2">
-                  <img src={listItem} className="pe-2"/>
-                  <span className="fw-normal">Tomate unos segundos para recuperar la calma.</span>
-                </li>
-                <li className="ms-2">
-                  <img src={listItem} className="pe-2"/>
-                  <span className="fw-normal">Apagá el motor y encendé las balizas.</span>
-                </li>
-                <li className="ms-2">
-                  <img src={listItem} className="pe-2"/>
-                  <span className="fw-normal">Corrobora que los pasajeros del otro vehículo se encentren bien. Llamá a emergencias si es necesario.</span>
-                </li>
-                <li className="ms-2">
-                  <img src={listItem} className="pe-2"/>
-                  <span className="fw-normal">Tené a mano <i>carnet de manejo, documentos del vehículo, DNI y Poliza de tu seguro o bien tarjeta de circulacion</i>. Pedí lo mismo al otro conductor.</span>
-                </li>
-                <li className="ms-2">
-                  <img src={listItem} className="pe-2"/>
-                  <span className="fw-normal">Tomá nota de la hora, fecha, lugar de ocurrencia, sentido de circulación de los vehículos y fotos del siniestro.</span>
-                </li>
-                <li className="ms-2">
-                  <img src={listItem} className="pe-2"/>
-                  <span className="fw-normal">Una vez en tu domicilio realiza la denuncia al 0800 de tu compañía, recordá que tenes 48 hs para realizarla.</span>
-                </li>
-              </ul>
+              <div className="ms-2">
+                <img src={listItem} className="pe-2"/>
+                <span className="fs-6 fw-light">Comprobá que los pasajeros de tu auto no tengan lesiones.</span>
+              </div>
+              <div className="ms-2">
+                <img src={listItem} className="pe-2"/>
+                <span className="fw-light">Tomate unos segundos para recuperar la calma.</span>
+              </div>
+              <div className="ms-2">
+                <img src={listItem} className="pe-2"/>
+                <span className="fw-light">Apagá el motor y encendé las balizas.</span>
+              </div>
+              <div className="ms-2">
+                <img src={listItem} className="pe-2"/>
+                <span className="fw-light">Corrobora que los pasajeros del otro vehículo se encentren bien. Llamá a emergencias si es necesario.</span>
+              </div>
+              <div className="ms-2">
+                <img src={listItem} className="pe-2"/>
+                <span className="fw-light">Tené a mano <i>carnet de manejo, documentos del vehículo, DNI y Poliza de tu seguro o bien tarjeta de circulacion</i>. Pedí lo mismo al otro conductor.</span>
+              </div>
+              <div className="ms-2">
+                <img src={listItem} className="pe-2"/>
+                <span className="fw-light">Tomá nota de la hora, fecha, lugar de ocurrencia, sentido de circulación de los vehículos y fotos del siniestro.</span>
+              </div>
+              <div className="ms-2">
+                <img src={listItem} className="pe-2"/>
+                <span className="fw-light">Una vez en tu domicilio realiza la denuncia al 0800 de tu compañía, recordá que tenes 48 hs para realizarla.</span>
+              </div>
             </div>
           </div>
         </div>
