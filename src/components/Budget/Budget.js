66ab22540248607f1604352e1fd2ca3585b416c9
cajimenez96 <carlosj_utn@hotmail.com>
2022-03-31 14:55:56 -0300

    fix budget

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -2,23 +2,22 @@ import React from 'react';
 
 const Budget = () => {
   return (
-    <div className='container'>
-      <div className='d-flex justify-content-between row m-0 pt-4'>
-        <div className='col-xl-5 col-12 p-0'>
-          <h2 className='mb-4'>DATOS DE CONTACTO</h2>
-          <div className="card">
-            <ul className="list-group list-group-flush p-2">
-              <li className="list-group-item">Correo electrónico: <a href="/#">perito.agomez@gmail.com</a></li>
-            </ul>
-            <span className='text-center p-3'>Redes Sociales</span>
-            <div className="card-body d-flex justify-content-evenly">
+    <div className="container d-flex">
+        <div className="col-xl-5 col-12 m-5 card">
+          <div className="card-title">
+            <h2 className="mb-4 ps-2 card-title fs-1 display-6">MEDIOS DE CONTACTO</h2>
+          </div>
+          <div className="card-body">
+              <span className="list-group-item">Correo electrónico: <i>perito.agomez@gmail.com</i></span>
+            </div>
+            <div className="card-footer">
+              <span className="text-center p-3">Redes Sociales</span>
               <a href="/#" className="card-link text-decoration-none text-dark">Facebook</a>
               <a href="/#" className="card-link text-decoration-none text-dark">Instagram</a>
             </div>
           </div>
-        </div>
-        <div className='col-xl-5 col-12'>
-          <h2 className='mb-4'>FORMULARIO DE CONTACTO</h2>
+        <div className="col-xl-4 col-12 m-5">
+          <h2 className="mb-4 fs-2 display-6">Solicitud de presupuesto</h2>
           <div className="mb-3">
             <label htmlFor="fullName" className="form-label">Nombre Completo</label>
             <input type="text" className="form-control" id="fullName" placeholder="Ingrese su nombre completo" />
@@ -27,32 +26,25 @@ const Budget = () => {
             <label htmlFor="email" className="form-label">Correo Electrónico</label>
             <input type="email" className="form-control" id="email" placeholder="Ingrese su correo electrónico" />
           </div>
-          <div className='row justify-content-between'>
-            <div className='col-lg-6 mb-3'>
+          <div className="row justify-content-between">
+            <div className="col-lg-6 mb-3">
               <label htmlFor="tel" className="form-label">Teléfono</label>
               <input type="tel" className="form-control" id="tel" placeholder="Ingrese su número de teléfono" />
             </div>
-            <div className='col-lg-4 mb-3'>
+            <div className="col-lg-4 mb-3">
               <label htmlFor="time" className="form-label">Horario de contacto</label>
               <input type="time" className="form-control" id="time" placeholder="Ingrese su número de teléfono" />
             </div>
           </div>
           <div className="mb-3">
             <label htmlFor="matter" className="form-label">Asunto</label>
-            <select className="form-select" aria-label="Default select example" defaultValue={'default'}>
-              <option value="default" disabled>Seleccione el motivo de su consulta</option>
-              <option value="1">Presupuesto</option>
-              <option value="2">No tengo seguro</option>
-              <option value="3">Otras</option>
-            </select>
           </div>
           <div className="mb-3">
             <label htmlFor="consult" className="form-label">Consulta</label>
-            <textarea className="form-control" id="consult" rows="3" placeholder='Ingrese su consulta'></textarea>
+            <textarea className="form-control" id="consult" rows="3" placeholder="Ingrese su consulta"></textarea>
           </div>
           <button type="button" className="btn btn-success">Enviar</button>
         </div>
-      </div>
     </div>
   );
 };
