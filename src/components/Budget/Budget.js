f4aa3360cd40eeb8f8849e898cbb63d41da43974
cajimenez96 <carlosj_utn@hotmail.com>
2022-04-08 16:13:39 -0300

    create component forms

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Forms } from '../Forms';
 
 const Budget = () => {
   return (
@@ -25,34 +26,7 @@ const Budget = () => {
           </div>
         </div>
       </div>
-      <div className="col-md-6 p-5 mt-5">
-        <h2 className="mb-4 fs-2 display-6">Solicitud de presupuesto</h2>
-        <div className="mb-3">
-          <label htmlFor="fullName" className="form-label">Nombre Completo</label>
-          <input type="text" className="form-control" id="fullName" placeholder="Ingrese su nombre completo" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Correo Electrónico</label>
-          <input type="email" className="form-control" id="email" placeholder="Ingrese su correo electrónico" />
-        </div>
-        <div className="row justify-content-between">
-          <div className="col-lg-6 mb-3">
-            <label htmlFor="tel" className="form-label">Teléfono</label>
-            <input type="tel" className="form-control" id="tel" placeholder="Ingrese su número de teléfono" />
-          </div>
-          <div className="col-lg-4 mb-3">
-            <label htmlFor="time" className="form-label">Horario de contacto</label>
-            <input type="time" className="form-control" id="time" placeholder="Ingrese su número de teléfono" disabled />
-          </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="consult" className="form-label">Consulta</label>
-          <textarea className="form-control" id="consult" rows="3" placeholder="Ingrese su consulta"></textarea>
-        </div>
-        <div className="text-lg-end text-sm-center">
-          <button type="button" className="btn btn-primary px-4">Solicitar presupuesto</button>
-        </div>
-      </div>
+      <Forms className="col-md-6" />
     </div>
   );
 };
