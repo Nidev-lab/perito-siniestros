b5ff94a83f0ef81acdd5602000d25840180cd9d3
cajimenez96 <carlosj_utn@hotmail.com>
2022-04-13 23:20:18 -0300

    update code responsive

--- src/components/FormsContact/FormsContact.js
+++ src/components/FormsContact/FormsContact.js
@@ -1,36 +1,40 @@
 import React from 'react';
+// import { useForm } from 'react-hook-form';
 
 const FormsContact = (props) => {
+  // const { register, handleSubmit } = useForm();
   return (
     <div className="col-md-6">
       <div className="p-md-5 mt-md-5">
         <h2 className="mb-4 fs-2 display-6">{props.tittle}</h2>
-        <div className="mb-3">
-          <label htmlFor="fullName" className="form-label">Nombre Completo</label>
-          <input type="text" className="form-control" id="fullName" placeholder="Ingrese su nombre completo" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Correo Electrónico</label>
-          <input type="email" className="form-control" id="email" placeholder="Ingrese su correo electrónico" />
-        </div>
-        <div className="row justify-content-between">
+        <form>
           <div className="mb-3">
-            <label htmlFor="tel" className="form-label">Teléfono</label>
-            <input type="number" className="form-control" id="tel" placeholder="Ingrese su número de teléfono" />
-            <span id="tel" className="form-text">Con código de área sin el 0 ni el 15. Ej: 3813999999</span>
+            <label htmlFor="fullName" className="form-label">Nombre Completo</label>
+            <input type="text" name="nombre" className="form-control" id="fullName" placeholder="Ingrese su nombre completo" required/>
           </div>
           <div className="mb-3">
-            <label htmlFor="time" className="form-label">Horario de contacto</label>
-            <input type="text" className="form-control" id="time" value="Lun. a Vie. 9hs-18hs y Sab. 9hs-12hs" disabled />
+            <label htmlFor="email" className="form-label">Correo Electrónico</label>
+            <input type="email" name="email" className="form-control" id="email" placeholder="Ingrese su correo electrónico" required/>
           </div>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="consult" className="form-label">Consulta</label>
-          <textarea className="form-control" id="consult" rows="3" placeholder="Ingrese su consulta"></textarea>
-        </div>
-        <div className="text-lg-end text-sm-center">
-          <button type="button" className="btn btn-primary px-4">{props.btnAction}</button>
-        </div>
+          <div className="row justify-content-between">
+            <div className="mb-3">
+              <label htmlFor="tel" className="form-label">Teléfono</label>
+              <input type="number" name="telefono" className="form-control" id="tel" placeholder="Ingrese su número de teléfono" required/>
+              <span id="tel" className="form-text">Con código de área sin el 0 ni el 15. Ej: 3813999999</span>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="time" className="form-label">Horario de contacto</label>
+              <input type="text" className="form-control" id="time" value="Lun. a Vie. 9hs-18hs y Sab. 9hs-12hs" disabled />
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="consult" className="form-label">Consulta</label>
+            <textarea className="form-control" name="comentarios" id="consult" rows="3" placeholder="Ingrese su consulta" required></textarea>
+          </div>
+          <div className="text-lg-end text-sm-center">
+            <input type="submit" className="btn btn-primary px-4" value={props.btnAction} />
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/components/ManageSinister/ManageSinister.js
+++ src/components/ManageSinister/ManageSinister.js
@@ -4,72 +4,60 @@ const ManageSinister = () => {
   return (
     <div className='container'>
       <h2 className='m-md-5 fs-1 display-6 text-center'>Deseo Gestionar un siniestro</h2>
-      <form className='row g-3 needs-validation' noValidate>
+      <form className='row g-3'>
         <div>
           <h5>Datos del tercero</h5>
         </div>
         <div className='col-md-5'>
           <label htmlFor='cia-tercero' className='form-label'>Compañia de seguro</label>
-          <input type='text' className='form-control' id='cia-tercero' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='ciaTer' className='form-control' id='cia-tercero' required />
         </div>
         <div className='col-md-5'>
           <label htmlFor='cobertura-tercero' className='form-label'>Cobertura</label>
-          <input type='text' className='form-control' id='cobertura-tercero' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='coberturaTer' className='form-control' id='cobertura-tercero' required />
         </div>
         <div className='col-md-5'>
           <label htmlFor='poliza-tercero' className='form-label'>Poliza de seguros</label>
-          <input type='text' className='form-control' id='poliza-tercero' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='polizaTer' className='form-control' id='poliza-tercero' required />
         </div>
         <hr/>
         <div className='col-md-3'>
           <label htmlFor='numero-siniestro' className='form-label'>Número de siniestro</label>
-          <input type='text' className='form-control' id='numero-siniestro' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='siniestroTer' className='form-control' id='numero-siniestro' required />
         </div>
         <div className='col-md-3'>
           <label htmlFor='numero-denuncia' className='form-label'>Número de denuncia</label>
-          <input type='text' className='form-control' id='numero-denuncia' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='denunciaTer' className='form-control' id='numero-denuncia' required />
         </div>
         <hr/>
         <div className='col-md-5'>
           <label htmlFor='nombre-tercero' className='form-label'>Nombre</label>
-          <input type='text' className='form-control' id='nombre-tercero' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='nombreTer' className='form-control' id='nombre-tercero' required />
         </div>
         <div className='col-md-5'>
           <label htmlFor='apellido-tercero' className='form-label'>Apellido</label>
-          <input type='text' className='form-control' id='apellido-tercero' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='apellidoTer' className='form-control' id='apellido-tercero' required />
         </div>
         <div className='col-md-3'>
           <label htmlFor='documento-tercero' className='form-label'>Documento</label>
-          <input type='text' className='form-control' id='documento-tercero' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='documentoTer' className='form-control' id='documento-tercero' required />
         </div>
         <div className='col-md-3'>
           <label htmlFor='domicilio-tercero' className='form-label'>Domicilio</label>
-          <input type='text' className='form-control' id='domicilio-tercero' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='domicilioTer' className='form-control' id='domicilio-tercero' required />
         </div>
         <div className='col-md-3'>
           <label htmlFor='telefono-tercero' className='form-label'>Telefono de contacto</label>
-          <input type='number' className='form-control' id='telefono-tercero' required />
+          <input type='number' name='telefonoTer' className='form-control' id='telefono-tercero' required />
           <span id="tel" className="form-text">Con código de área sin el 0 ni el 15. Ej: 3813999999</span>
-          <div className='valid-feedback'>Completo!</div>
         </div>
         <div className='col-md-5'>
           <label htmlFor='marca-tercero' className='form-label'>Marca del auto</label>
-          <input type='text' className='form-control' id='marca-tercero' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='marcaTer' className='form-control' id='marca-tercero' required />
         </div>
         <div className='col-md-5'>
           <label htmlFor='modelo-tercero' className='form-label'>Modelo del vehiculo</label>
-          <input type='text' className='form-control' id='modelo-tercero' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='modeloTer' className='form-control' id='modelo-tercero' required />
         </div>
         <hr/>
         <div>
@@ -77,49 +65,41 @@ const ManageSinister = () => {
         </div>
         <div className='col-md-5'>
           <label htmlFor='nombre-solicitante' className='form-label'>Nombre</label>
-          <input type='text' className='form-control' id='nombre-solicitante' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='nombreSol' className='form-control' id='nombre-solicitante' required />
         </div>
         <div className='col-md-5'>
           <label htmlFor='apellido-solicitante' className='form-label'>Apellido</label>
-          <input type='text' className='form-control' id='apellido-solicitante' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='apellidoSol' className='form-control' id='apellido-solicitante' required />
         </div>
         <div className='col-md-3'>
           <label htmlFor='documento-solicitante' className='form-label'>Documento</label>
-          <input type='text' className='form-control' id='documento-solicitante' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='documentoSol' className='form-control' id='documento-solicitante' required />
         </div>
         <div className='col-md-3'>
           <label htmlFor='domicilio-solicitante' className='form-label'>Domicilio</label>
-          <input type='text' className='form-control' id='domicilio-solicitante' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='domicilioSol' className='form-control' id='domicilio-solicitante' required />
         </div>
         <div className='col-md-3'>
           <label htmlFor='telefono-solicitante' className='form-label'>Telefono de contacto</label>
-          <input type='number' className='form-control' id='telefono-solicitante' required />
+          <input type='number' name='telefonoSol' className='form-control' id='telefono-solicitante' required />
           <span id="tel" className="form-text">Con código de área sin el 0 ni el 15. Ej: 3813999999</span>
-          <div className='valid-feedback'>Completo!</div>
         </div>
         <div className='col-md-5'>
           <label htmlFor='cia-solicitante' className='form-label'>Compañia de seguro</label>
-          <input type='text' className='form-control' id='cia-solicitante' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='ciaSol' className='form-control' id='cia-solicitante' required />
         </div>
         <div className='col-md-5'>
           <label htmlFor='cobertura-solicitante' className='form-label'>Cobertura</label>
-          <input type='text' className='form-control' id='cobertura-solicitante' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='coberturaSol' className='form-control' id='cobertura-solicitante' required />
         </div>
         <div className='col-md-5'>
           <label htmlFor='poliza-solicitante' className='form-label'>Poliza de seguros</label>
-          <input type='text' className='form-control' id='poliza-solicitante' required />
-          <div className='valid-feedback'>Completo!</div>
+          <input type='text' name='polizaSol' className='form-control' id='poliza-solicitante' required />
         </div>
         <hr/>
         <div className='col-md-7 me-md-3'>
           <label htmlFor='comentario' className='form-label'>Cuentanos lo sucedido</label>
-          <textarea className='form-control' id='comentario' rows='3' required></textarea>
+          <textarea className='form-control' name='comentarios' id='comentario' rows='3' required></textarea>
         </div>
         <div className='col-md-4 ms-md-3'>
           <label className='form-label'>Horario de contacto</label>
