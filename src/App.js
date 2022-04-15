184c26491a1a80f45f09d55702d5c04c4ff45888
Alejandro Busi <Alejandrobusi@gmail.com>
2022-04-15 19:42:02 -0300

    added loader component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
@@ -5,18 +6,31 @@ import { Home } from './pages/Home';
 import ManageSinisterPage from './pages/ManageSinister/ManageSinisterPage';
 import FreeRepair from './pages/FreeRepair/FreeRepairPage';
 import BudgetPage from './pages/Budget/BudgetPage';
+import { Loader } from './components/Loader';
 
 const App = () => {
+  const [timeOff, setTimeOff] = useState(true);
+
+  const timer = setTimeout(() => { setTimeOff(false); }, 3000);
+
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={ <Home /> } />
-        <Route path='/siniestro' element={ <ManageSinisterPage /> } />
-        <Route path='/reparar' element={ <FreeRepair /> } />
-        <Route path='/presupuesto' element={ <BudgetPage />} />
-      </Routes>
-      <Footer />
+      {
+        timeOff
+          ? <Loader />
+          : (
+            <>
+              <Navbar />
+                <Routes>
+                  <Route path='/' element={ <Home /> } />
+                  <Route path='/siniestro' element={ <ManageSinisterPage /> } />
+                  <Route path='/reparar' element={ <FreeRepair /> } />
+                  <Route path='/presupuesto' element={ <BudgetPage />} />
+                </Routes>
+              <Footer />
+            </>
+          )
+      }
     </BrowserRouter>
   );
 };
